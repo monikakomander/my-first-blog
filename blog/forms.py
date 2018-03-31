3aa471ce74fd9e2583e34eaefa84fb3ec308895c
monika <komander.monika@gmail.com>
2018-03-31 09:16:14 +0200

    Added views to create and edit blog post inside the side.

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+__author__ = 'monika'
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
